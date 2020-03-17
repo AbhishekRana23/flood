1e143bd1d96f2dc2dc1e5bfa66700b04b9dcd01d
Abhishek Rana <40822125+AbhishekRana23@users.noreply.github.com>
2020-03-17 12:44:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,53 +1,20 @@
-// This is the configuration file for Flood, a React-based frontend for the
-// rtorrent BitTorrent client.
-// Copy this file to ./config.js and make changes below.
-// config.js must exist before running `npm run build`.
-
 const CONFIG = {
-  // This URI will prefix all of Flood's HTTP requests. You _must_ have a web
-  // server, like nginx, configured to forward these requests to the Flood
-  // web server.
-  // For example, if you intend to serve from http://example.com/flood, set this to
-  // '/flood' and configure your web server to pass _all_ requests from `/flood` to
-  // the root of Flood's web server.
-  // Recompiling assets with `npm run build` is needed after each `baseURI` change.
-  // See https://github.com/Flood-UI/flood/wiki/Using-Flood-behind-a-reverse-proxy
-  baseURI: '/',
-  // Flood uses a local nedb database to keep track of users, torrents,
-  // and activity. The database is regularly purged to remove outdated data.
-  // This value dictates how old data is, in milliseconds, before being purged.
+  baseURI: process.env.FLOOD_BASE_URI || '/',
   dbCleanInterval: 1000 * 60 * 60,
-  // Where to store the local nedb database.
-  dbPath: './server/db/',
-  // The host that Flood should listen for web connections on.
-  // If you want to connect to Flood from hosts other that the one it is running
-  // on, you should change this value.
-  // To listen on all interfaces, change to `floodServerHost: '0.0.0.0'`..
-  floodServerHost: '0.0.0.0',
-  // The port that Flood should listen for web connections on.
+  dbPath: '/data/server/db/',
   floodServerPort: 3000,
-  // Used for development. See the "Local Development" section of README.md
-  // for detail.
-  floodServerProxy: 'https://floodr.herokuapp.com/',
-  // Flood keeps a history of torrent download and upload speeds.
-  // This value dictates the number of individual records per period to keep.
   maxHistoryStates: 30,
-  // How often (in milliseconds) Flood will request the torrent list from.
-  torrentClientPollInterval: 1000 * 2,
-  // A unique secret for signing messages with JWT (see https://jwt.io). Change
-  // this to something unique and hard to guess.
-  secret: 'flood',
-  // Configuration for SSL, if using SSL with the Flood service directly.
-  ssl: false,
-  sslKey: '/absolute/path/to/key/',
-  sslCert: '/absolute/path/to/certificate/',
-  // disk space service checks disk space of mounted partitions
-  diskUsageService: {
-    // assign desired mounts to include. Refer to "Mounted on" column of `df -P`
-    // watchMountPoints: [
-    //   "/mnt/disk"
-    // ]
-  }
+  pollInterval: 1000 * 5,
+  secret: process.env.FLOOD_SECRET || 'flood',
+  scgi: {
+    host: process.env.RTORRENT_SCGI_HOST || 'localhost',
+    port: process.env.RTORRENT_SCGI_PORT || 5000,
+    socket: process.env.RTORRENT_SOCK === 'true' || process.env.RTORRENT_SOCK === true,
+    socketPath: process.env.RTORRENT_SOCK_PATH || '/data/rtorrent.sock',
+  },
+  ssl: process.env.FLOOD_ENABLE_SSL === 'true' || process.env.FLOOD_ENABLE_SSL === true,
+  sslKey: '/data/flood_ssl.key',
+  sslCert: '/data/flood_ssl.cert',
 };
-// Do not remove the below line.
+
 module.exports = CONFIG;
